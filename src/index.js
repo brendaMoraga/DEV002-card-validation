d3b02bcc6237e75e5725348bcf46c9ec6ddaede7
Brenda Moraga <Brenda@MacBook-Pro-de-Brenda.local>
2022-11-03 17:43:53 -0300

    primera version con reajustes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import validator from './validator.js';
 
-console.log(validator);
+const elCCNumber = document.getElementById('ccNumber');
+const elCCValidation = document.getElementById('ccValidator');
+const checkCC = (text) => {
+  let message = "";
+  console.log(text)
+  if (validator.isValid(text))
+    message = "Número de tarjeta válido";
+  
+    //message1.style.color = "green"
+  else
+    message = "Tu tarjeta es inválida, revisa los digitos ingresados o consulta con tu proveedor";
+
+
+
+  elCCValidation.textContent = message;
+
+};
+
+const btn = document.getElementById('btn-validar')
+btn.addEventListener("click", function () {
+  checkCC(elCCNumber.value);
+
+  validator.maskify(elCCNumber.value);
+});
+
+
+
+
+//5457623898234113
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
 const validator = {
-  // ...
+    isValid: (ccNumber) => {
+        const arrNumber = ccNumber.split("").reverse()
+        console.log(arrNumber)
+        let count = 0;
+
+        for (let i = 0; i < arrNumber.length; i++) {
+            let currentDigit = parseInt(arrNumber[i]);
+            if (i % 2 == 0) {
+                if ((currentDigit *= 2) > 9) {
+
+
+                } else {
+
+                    (currentDigit >=10) // -> sume los digitos de los numeros mayores a 10 guardar numeros multiplicados por 2 < 9
+                }
+
+            } else {
+                // 
+            }
+
+            count += currentDigit;
+        }
+    },
+
+    maskify: (ccNumber) => {
+        if (ccNumber.length < 4) {
+            return ccNumber
+        } else {
+            let last4 = ""
+            for (let i = ccNumber.length - 4; i < ccNumber.length; i++) {
+                last4 += ccNumber[i]
+            }
+
+            let mask = ""
+            for (let j = 0; j < ccNumber.length - 4; j++) {
+                mask += "#"
+            }
+            return mask + last4
+        }
+    }
+
 };
 
+
+
+
+
+
+
+
+
 export default validator;
+
+
